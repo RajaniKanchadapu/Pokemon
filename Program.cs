04ec38c4e347be1470c8af334f95481580e692a5
RajaniKanchadapu <64625913+RajaniKanchadapu@users.noreply.github.com>
2022-11-23 13:42:52 -0600

    Add files via upload
    Uploaded the project files.

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿
+using System.Linq;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using HttpClient client = new();
+
+        Console.WriteLine("Enter Pokemon name: ");
+
+        var typeName =  Console.ReadLine();
+
+        if(string.IsNullOrEmpty(typeName))
+        {
+            Console.WriteLine("Name is empty");
+        }
+        else
+        {
+            typeName = typeName.Trim().ToLower();
+            await GetStrengthsAndWeaknesses(client, typeName);
+        }  
+       
+
+        async Task GetStrengthsAndWeaknesses(HttpClient client, string name)
+        { 
+            try
+            {
+                var json = await client.GetStringAsync("https://pokeapi.co/api/v2/type/" + name);
+
+                var root = GetRoot(json);
+
+                if (root != null && root.damage_relations != null)
+                {
+                    DsiplayStrengthsAndWeaknesses(root.damage_relations, root.name.ToUpper());
+                }
+                else
+                {
+                    Console.WriteLine("The entered type is wrong.");
+                }
+            } 
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("-----------------");
+                Console.Out.WriteLine(e.Message);
+            }
+            
+        }
+
+        void DsiplayStrengthsAndWeaknesses(DamageRelations damageRelations, string typeName)
+        {
+            DisplayStrengths(damageRelations, typeName);
+
+            DisplayWeaknesses(damageRelations, typeName);
+        }
+    } 
+
+    private static void DisplayWeaknesses(DamageRelations damageRelations, string typeName)
+    {
+        if (damageRelations.double_damage_from.Count > 0 || damageRelations.no_damage_to.Count > 0)
+        {
+            Console.WriteLine($"The following are the Weaknesses of type:{typeName}");
+            if (damageRelations.double_damage_from.Count > 0)
+            {
+                foreach (var weakness in damageRelations.double_damage_from)
+                {
+                    Console.WriteLine(weakness.name.ToUpper());
+                }
+            }
+            if (damageRelations.no_damage_to.Count > 0)
+            {               
+                foreach (var weakness in damageRelations.no_damage_to)
+                {
+                    Console.WriteLine(weakness.name.ToUpper());
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine($"No Weaknesses Found for type: {typeName}");
+        }
+    }
+
+    private static void DisplayStrengths(DamageRelations damageRelations, string typeName)
+    {
+        if (damageRelations.double_damage_to.Count > 0 || damageRelations.no_damage_from.Count > 0)
+        {
+            Console.WriteLine($"The following are the Strengths of type:{typeName}");
+            if(damageRelations.double_damage_to.Count > 0)
+            {
+                foreach (var strength in damageRelations.double_damage_to)
+                {
+                    Console.WriteLine(strength.name.ToUpper());
+                }
+            }
+           if (damageRelations.no_damage_from.Count > 0)
+            {
+                foreach (var strength in damageRelations.no_damage_from)
+                {
+                    Console.WriteLine(strength.name.ToUpper());
+                }
+            } 
+        }
+        else
+        {
+            Console.WriteLine($"No Strengths Found for the type: {typeName}");
+        }
+    } 
+    private static Root? GetRoot(string json)
+    {
+        return System.Text.Json.JsonSerializer.Deserialize<Root>(json);
+    }
+}
---
+++ Root.cs
@@ -0,0 +1,111 @@
+﻿
+public class DamageRelations
+{
+    public List<DoubleDamageFrom> double_damage_from { get; set; }
+    public List<DoubleDamageTo> double_damage_to { get; set; }
+    public List<HalfDamageFrom> half_damage_from { get; set; }
+    public List<HalfDamageTo> half_damage_to { get; set; }
+    public List<NoDamageFrom> no_damage_from { get; set; }
+    public List<NoDamageTo> no_damage_to { get; set; }
+}
+
+public class DoubleDamageFrom
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class DoubleDamageTo
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class GameIndex
+{
+    public int game_index { get; set; }
+    public Generation generation { get; set; }
+}
+
+public class Generation
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class HalfDamageFrom
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class HalfDamageTo
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class NoDamageFrom
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class NoDamageTo
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class Language
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class Move
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class MoveDamageClass
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class Name
+{
+    public Language language { get; set; }
+    public string name { get; set; }
+}
+
+public class PastDamageRelation
+{
+    public DamageRelations damage_relations { get; set; }
+    public Generation generation { get; set; }
+}
+
+public class Pokemon
+{
+    public Pokemon pokemon { get; set; }
+    public int slot { get; set; }
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class Root
+{
+    public DamageRelations damage_relations { get; set; }
+    public List<GameIndex> game_indices { get; set; }
+    public Generation generation { get; set; }
+    public int id { get; set; }
+    public MoveDamageClass move_damage_class { get; set; }
+    public List<Move> moves { get; set; }
+    public string name { get; set; }
+    public List<Name> names { get; set; }
+    public List<PastDamageRelation> past_damage_relations { get; set; }
+    public List<Pokemon> pokemon { get; set; }
+}
+
